d1f92dc393767c40259d680c62ef1e54bfeb409a
SoumyaCMC <soumya133.ranjan@gmail.com>
2021-04-23 19:25:23 +0530

    new teams

--- team.js
+++ team.js
@@ -27,3 +27,18 @@ window.onscroll = () => {
         }
     }
 };
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    removeActiveClass();
+    card.classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+}
